6d5bfe03ed62c582368cab3bab819e6722c3f43b
liziks <9855767580@mail.ru>
2026-04-01 00:00:56 +0300

    Добавьте файлы проекта.

---
+++ Alignment/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Alignment")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Alignment.Pages")]
---
+++ Alignment/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace Alignment;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    void OnHorizontalStart(object sender, EventArgs e) =>
+        DemoLabel.HorizontalOptions = LayoutOptions.Start;
+
+    void OnHorizontalCenter(object sender, EventArgs e) =>
+        DemoLabel.HorizontalOptions = LayoutOptions.Center;
+
+    void OnHorizontalEnd(object sender, EventArgs e) =>
+        DemoLabel.HorizontalOptions = LayoutOptions.End;
+
+    void OnHorizontalFill(object sender, EventArgs e) =>
+        DemoLabel.HorizontalOptions = LayoutOptions.Fill;
+
+    void OnVerticalStart(object sender, EventArgs e) =>
+        DemoLabel.VerticalOptions = LayoutOptions.Start;
+
+    void OnVerticalCenter(object sender, EventArgs e) =>
+        DemoLabel.VerticalOptions = LayoutOptions.Center;
+
+    void OnVerticalEnd(object sender, EventArgs e) =>
+        DemoLabel.VerticalOptions = LayoutOptions.End;
+
+    void OnVerticalFill(object sender, EventArgs e) =>
+        DemoLabel.VerticalOptions = LayoutOptions.Fill;
+}
